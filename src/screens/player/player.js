9abaa633cc604be666f8710dae2bedaf7d54086f
Talia Betourney <156220495+tebetourney@users.noreply.github.com>
2024-04-24 10:30:26 -0700

    getting the player page set up w/ song card

--- src/screens/player/player.js
+++ src/screens/player/player.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./player.css";
+import { useLocation } from 'react-router-dom';
+import apiClient from '../../spotify';
+import Queue from '../../components/queue/queue';
+import SongCard from '../../components/songCard/songCard';
 
 export default function Player() {
+  
+  const location = useLocation();
+  const [tracks, setTracks] = useState([]);
+  const [currentTrack, setCurrentTrack] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if(location.state){
+      apiClient.get("playlists/" + location.state?.id + "/tracks")
+      .then((res)=> {
+        setTracks(res.data.items);
+        setCurrentTrack(res.data.items[0].track);
+      });
+    }
+  },[location.state]);
+  
   return (
-    <div className="screen-container">Player</div>
+    <div className="screen-container flex">
+      <div className="left-player-body">
+
+      </div>
+      <div className="right-player-body">
+        <SongCard album={currentTrack.album}/>
+        <Queue />
+      </div>
+    </div>
   )
 }
