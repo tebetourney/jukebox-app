9a58bb56b20cfa6192b169cd501cb35e4dacf2a4
Talia Betourney <156220495+tebetourney@users.noreply.github.com>
2024-04-30 11:37:34 -0700

    fixing some wonky errors with pause/play states

--- src/components/audioPlayer/audioPlayer.js
+++ src/components/audioPlayer/audioPlayer.js
@@ -1,128 +1,124 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import "./audioPlayer.css";
 import ProgressCircle from './progressCircle';
 import WaveAnimation from './waveAnimation';
 import Controls from './controls';
 
-
-export default function AudioPlayer({currentTrack, currentIndex, setCurrentIndex, total}) {
-    
-    const [isPlaying, setIsPlaying] = useState(true);
-    const [trackProgress, setTrackProgress] = useState(0);
-    var audioSrc = total[currentIndex]?.track.preview_url;
-
-    const audioRef = useRef(new Audio(total[currentIndex]?.track.preview_url));
-
-    const intervalRef = useRef();
-
-    const isReady = useRef(false);
-
-    const { duration } = audioRef.current;
-
-    const currentPercentage = duration ? (trackProgress / duration) * 100 : 0;
-
-    const startTimer = () => {
-        clearInterval(intervalRef.current);
-        intervalRef.current = setInterval(() => {
-            if(audioRef.current.ended){
-                handleNext();
-            }
-            else {
-                setTrackProgress(audioRef.current.currentTime);
-            }
-        }, [1000]);
-    };
-
-    useEffect(() => {
-        if(isPlaying && audioRef.current) {
-            audioRef.current = new Audio(audioSrc);
-            audioRef.current.play();
-            startTimer();
-        }
-        else {
-            clearInterval(intervalRef.current);
-            audioRef.current.pause();
-        }
-    }, [isPlaying]);
-
-    useEffect(() => {
-        audioRef.current.pause();
-        audioRef.current = new Audio(audioSrc);
-        setTrackProgress(audioRef.current.currentTime);
-
-        if(isReady.current) {
-            audioRef.current.play();
-            setIsPlaying(true);
-            startTimer(); 
-        }
-        else {
-            isReady.current = true;
-        }       
-    }, [currentIndex]);
-
-    useEffect(() => {
-        return () => {
-            audioRef.current.pause();
-            clearInterval(intervalRef.current);
-        };
-    }, []);
-
-    const handleNext = () => {
-        if (currentIndex < total.length - 1){
-            setCurrentIndex(currentIndex + 1);
-        }
-        else {
-            setCurrentIndex(0);
-        }
-    };
-
-    const handlePrev = () => {
-        if (currentIndex - 1 < 0) {
-            setCurrentIndex(total.length -1);
-        }
-        else {
-            currentIndex(currentIndex -1);
-        }
-    };
-
-    const addZero = (n) => {
-        return n > 9 ? "" + n : "0" + n;
-    }
-    
-    const artists = [];
-    currentTrack?.album?.artists.forEach((artist) => {
-        artists.push(artist.name);
-    });
+export default function AudioPlayer({ currentTrack, currentIndex, setCurrentIndex, total }) {
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [trackProgress, setTrackProgress] = useState(0);
   
-    return (
+  const audioRef = useRef(new Audio(total[currentIndex]?.track.preview_url)); 
+
+  const intervalRef = useRef();
+  const isReady = useRef(false);
+
+  const { duration } = audioRef.current;
+  const currentPercentage = duration ? (trackProgress / duration) * 100 : 0;
+
+  intervalRef.current = setInterval(() => {
+    if (audioRef.current.ended) {
+      handleNext();
+    } else {
+      console.log("Current time:", audioRef.current.currentTime); 
+      setTrackProgress(audioRef.current.currentTime); 
+    }
+  }, 1000);
+  
+
+  const startTimer = () => {
+    clearInterval(intervalRef.current); 
+    intervalRef.current = setInterval(() => {
+      if (audioRef.current.ended) {
+        handleNext(); 
+      } else {
+        setTrackProgress(audioRef.current.currentTime); 
+      }
+    }, 1000); 
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play().catch((err) => console.error("Play interrupted:", err)); 
+      startTimer(); 
+    } else {
+      clearInterval(intervalRef.current); 
+      audioRef.current.pause(); 
+    }
+  }, [isPlaying]); 
+
+  useEffect(() => {
+    audioRef.current.pause(); 
+    audioRef.current.src = total[currentIndex]?.track.preview_url; 
+    setTrackProgress(0); 
+
+    if (isReady.current) {
+      audioRef.current.play().catch((err) => console.error("Play interrupted:", err)); 
+      setIsPlaying(true); 
+      startTimer(); 
+    } else {
+      isReady.current = true; 
+    }
+  }, [currentIndex]); 
+
+  useEffect(() => {
+    return () => {
+      audioRef.current.pause(); 
+      clearInterval(intervalRef.current); 
+    };
+  }, []); 
+
+  const handleNext = () => {
+    if (currentIndex < total.length - 1) {
+      setCurrentIndex(currentIndex + 1); 
+    } else {
+      setCurrentIndex(0); 
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentIndex - 1 < 0) {
+      setCurrentIndex(total.length - 1); 
+    } else {
+      setCurrentIndex(currentIndex - 1); 
+    }
+  };
+
+  const addZero = (n) => {
+    return n > 9 ? "" + n : "0" + n; 
+  };
+
+  const artists = currentTrack?.album?.artists.map((artist) => artist.name).join(" | ");
+
+  return (
     <div className="player-body flex">
-        <div className="player-left-body">
-            <ProgressCircle 
-                percentage = {currentPercentage}
-                isPlaying = {true}
-                image = {currentTrack?.album?.images[0]?.url}
-                size = {300}
-                color = "#895d2b"
-            />
+      <div className="player-left-body">
+        <ProgressCircle 
+          percentage={currentPercentage}
+          isPlaying={isPlaying}
+          image={currentTrack?.album?.images[0]?.url}
+          size={300}
+          color="#895d2b"
+        />
+      </div>
+      <div className="player-right-body flex">
+        <p className="song-title">{currentTrack?.name}</p>
+        <p className='song-artist'>{artists}</p>
+        <div className="player-right-bottom flex">
+          <div className="song-duration flex">
+            <p className="duration">0:{addZero(Math.round(trackProgress))}</p>
+            <WaveAnimation isPlaying={isPlaying} />
+            <p className="duration">0:30</p>
+          </div>
+          <Controls 
+            isPlaying={isPlaying}
+            setIsPlaying={setIsPlaying}
+            handleNext={handleNext}
+            handlePrev={handlePrev}
+            total={total}
+          />
         </div>
-        <div className="player-right-body flex">
-            <p className="song-title">{currentTrack?.name}</p>
-            <p className='song-artist'>{artists.join(" | ")}</p>
-            <div className="player-right-bottom flex">
-                <div className="song-duration flex">
-                    <p className="duration">0:{addZero(Math.round(trackProgress))}</p>
-                    <WaveAnimation isPlaying={isPlaying} />
-                    <p className="duration">0:30</p>
-                </div>
-                <Controls 
-                    isPlaying={isPlaying}
-                    setIsPlaying={setIsPlaying}
-                    handleNext={handleNext}
-                    handlePrev={handlePrev}
-                    total={total} 
-                />
-            </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/audioPlayer/controls.js
+++ src/components/audioPlayer/controls.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import "./controls.css";
+import { IconContext } from 'react-icons';
+import { IoPause, IoPlay, IoPlaySkipBack, IoPlaySkipForward } from 'react-icons/io5';
 
-export default function Controls() {
-  return (
-    <div>
-      Controls
-    </div>
-  )
+export default function Controls({
+  isPlaying, 
+  setIsPlaying, 
+  handleNext, 
+  handlePrev
+}) {
+  return ( 
+  <IconContext.Provider value={{size: "35px", color: "#cbb89d"}}>
+    <div className="controls-wrapper">
+      <div className='action-btn' onClick={handlePrev}>
+        <IoPlaySkipBack />
+      </div>
+      <div className="play-pause-btn" onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? <IoPause /> : <IoPlay />}
+      </div>
+      <div className='action-btn' onClick={handleNext}>
+        <IoPlaySkipForward />
+      </div>
+    </div>;
+  </IconContext.Provider>
+  );
 }
